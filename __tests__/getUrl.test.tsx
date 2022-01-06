76b1510a1baf5144d6f287d94c37988b4190f215
yjj <jmyoon8@naver.com>
2022-01-06 18:31:19 +0900

    모든 분기 테스트 완료

--- __tests__/getUrl.test.tsx
+++ __tests__/getUrl.test.tsx
@@ -5,22 +5,55 @@ import {getParsingQuizOption} from '../modules/utils/utilFunction';
  */
 describe('functionTest', () => {
    test('getParsingQuizOption return UrlParam?', () => {
-      const getParsingQuizOptionParams = {
+      const getParsingQuizOptionParamsEasy = {
          difficulty: '쉬움',
          numberOfQuiz: 1,
          quizType: '객관식',
          selectedCategory: '일반상식',
       };
-
-      const getParsingData = getParsingQuizOption(
-         getParsingQuizOptionParams.selectedCategory,
-         getParsingQuizOptionParams.difficulty,
-         getParsingQuizOptionParams.quizType,
-         getParsingQuizOptionParams.numberOfQuiz,
+      const getParsingDataEasy = getParsingQuizOption(
+         getParsingQuizOptionParamsEasy.selectedCategory,
+         getParsingQuizOptionParamsEasy.difficulty,
+         getParsingQuizOptionParamsEasy.quizType,
+         getParsingQuizOptionParamsEasy.numberOfQuiz,
       );
-      expect(getParsingData.amount).toBe(10);
-      expect(getParsingData.category).toBe(9);
-      expect(getParsingData.difficulty).toBe('easy');
-      expect(getParsingData.type).toBe('multiple');
+      expect(getParsingDataEasy.amount).toBe(10);
+      expect(getParsingDataEasy.category).toBe(9);
+      expect(getParsingDataEasy.difficulty).toBe('easy');
+      expect(getParsingDataEasy.type).toBe('multiple');
+
+      const getParsingQuizOptionParamsMedium = {
+         difficulty: '보통',
+         numberOfQuiz: 1,
+         quizType: '객관식',
+         selectedCategory: '일반상식',
+      };
+      const getParsingDataMedium = getParsingQuizOption(
+         getParsingQuizOptionParamsMedium.selectedCategory,
+         getParsingQuizOptionParamsMedium.difficulty,
+         getParsingQuizOptionParamsMedium.quizType,
+         getParsingQuizOptionParamsMedium.numberOfQuiz,
+      );
+      expect(getParsingDataMedium.amount).toBe(10);
+      expect(getParsingDataMedium.category).toBe(9);
+      expect(getParsingDataMedium.difficulty).toBe('medium');
+      expect(getParsingDataMedium.type).toBe('multiple');
+
+      const getParsingQuizOptionParamsHard = {
+         difficulty: '어려움',
+         numberOfQuiz: 1,
+         quizType: '객관식',
+         selectedCategory: '일반상식',
+      };
+      const getParsingDataHard = getParsingQuizOption(
+         getParsingQuizOptionParamsHard.selectedCategory,
+         getParsingQuizOptionParamsHard.difficulty,
+         getParsingQuizOptionParamsHard.quizType,
+         getParsingQuizOptionParamsHard.numberOfQuiz,
+      );
+      expect(getParsingDataHard.amount).toBe(10);
+      expect(getParsingDataHard.category).toBe(9);
+      expect(getParsingDataHard.difficulty).toBe('hard');
+      expect(getParsingDataHard.type).toBe('multiple');
    });
 });
